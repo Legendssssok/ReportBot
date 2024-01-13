86f9117a1131dc01b00f21c67bf647d3ba494d40
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-13 18:59:49 +0530

    Update __init__.py

--- Report/Config/__init__.py
+++ Report/Config/__init__.py
@@ -3,6 +3,6 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from sample_config import *  # noqa
+    from sample_config import Config  # noqa
 elif os.path.exists("config.py"):
-    from config import *
+    from config import Config
