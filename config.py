d05cd97cf87e34fa7b49916b012d777ebe360931
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-13 18:59:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+class Config(object):
+    # Pyrogram Client
+    API_ID    =  11573285 # ⚠️ Required
+    API_HASH  = "f2cc3fdc32197c8fbaae9d0bf69d2033" # ⚠️ Required
+    BOT_TOKEN =  "6881298907:AAEUslppizLWBMCS6AwF0x4CtuTzVtPp47s"# ⚠️ Required
+    
+    # Other Configs
+    BOT_START_TIME = time.time()
+    OWNER = "5591734243" # ⚠️ Required
+    SUDO = list(map(int, os.environ.get("SUDO", "5591734243").split()))  # ⚠️ Required
+    # Web Response Config
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    PORT = int(os.environ.get("PORT", "8080"))
